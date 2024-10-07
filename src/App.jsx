f2fca1a5a15639e64c6b17f4c926a0cac32c6aec
ZiyeW-zz <125829074+ZiyeW-zz@users.noreply.github.com>
2024-10-06 23:47:45 -0500

    Fetch data task complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,38 @@
+import React from 'react';  
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import ProductList from './ProductList';
+import { useJsonQuery } from './utilities/fetch'; // custom hook
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
-
+const queryClient = new QueryClient();
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  const [coursesData, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading courses: {error.message}</div>;
+
+  const courses = coursesData.courses;
 
   return (
     <div>
-      <div className="ListCourses">
-        <Banner title={schedule.title} />
-        <CourseList courses={schedule.courses} />
-        {/* Convert courses object to an array and pass to ProductList */}
-      </div>
 
       <div className="">
-        <ProductList products={Object.values(schedule.courses)} />
+        <ProductList products={Object.values(courses)} />
       </div>
     </div>
   );
 };
 
+const Root = () => (
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
 
-export default App;
+export default Root;
